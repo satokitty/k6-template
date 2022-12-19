36e8bbd819c1506d5c57868927c20af28a9a3aa7
satokitty <tomohiko.s@gmail.com>
2022-12-19 17:23:10 +0900

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # k6-template
+
+## 構成
+
+- src
+    - steps
+    - flows
+    - scenarios
+        - smoke
+        - performance
+        - stability
+
+### step
+
+リクエストとレスポンス検証をまとめたモジュール。
+この単位でシナリオを構築する。
+
+### flow
+
+stepを組み合わせたテストの一連の流れを表現したモジュール。
+
+### scenario
+
+シナリオのフローとその実行設定を組み合わせたモジュール。k6のシナリオ。
+
+```mermaid
+classDiagram
+
+Scenario "1" --> "*" Flow
+Flow "1" --> "*" Step
+```
+
+## build/run
+
+```shell
+$ yarn webpack
+$ k6 ./dist/scenarios/scenario-test-1.js
+```
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const glob = require('glob');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+function globEntries(basePath, globPath) {
+  const re = new RegExp(`${basePath}/`);
+  const files = glob.sync(globPath);
+  return Object.fromEntries(files.map(entry => {
+    return [
+      entry.replace(re, '').replace(path.extname(entry), ''),
+      entry
+    ]
+  }));
+}
+
+module.exports = {
+  mode: 'production',
+  entry: globEntries('src', './src/scenarios/**/*test*.ts'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    libraryTarget: 'commonjs',
+    filename: '[name].js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  target: 'web',
+  externals: /^(k6|https?\:\/\/)(\/.*)?/,
+  // Generate map files for compiled scripts
+  devtool: "source-map",
+  stats: {
+    colors: true,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    // Copy assets to the destination folder
+    // see `src/post-file-test.ts` for an test example using an asset
+    new CopyPlugin({
+      patterns: [{
+        from: path.resolve(__dirname, 'assets'),
+        noErrorOnMissing: true
+      }],
+    }),
+  ],
+  optimization: {
+    // Don't minimize, as it's not used in the browser
+    minimize: false,
+  },
+};
